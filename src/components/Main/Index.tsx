5d92561070c5cdf6cc2c14bdbc0485ca811e1b8a
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-02-18 18:16:57 -0300

    refactor: reajuste na logica da main

--- src/components/Main/Index.tsx
+++ src/components/Main/Index.tsx
@@ -4,21 +4,57 @@ import thaisImg from "../../assets/ThaísGomes.jpg";
 import RicardoImg from "../../assets/RicardoSiqueira.jpg";
 import Banner from "../Banner"
 
+type Post = {
+  id: number
+  image: string
+  author: string
+  occupation: string
+  text1: string
+  text2: string
+  text3: string
+}
+
 export default function Main(){
+    const posts: Post[] = [
+        {
+            id: 1,
+            image: thaisImg,
+            author: "Thaís Gomes",
+            occupation: "Designer",
+            text1: "Lorem ipsum ",
+            text2: "dolor sit amet. Ex laboriosam dolorem non tempore earum et voluptatem suscipit ut cupiditate nisi est odit voluptates. Nam magni amet ut ipsam molestiae aut facilis minus et quia reiciendis sed excepturi rerum ex consequatur minima! Ex rerum sunt et incidunt officia et veritatis deserunt. Sit soluta laboriosam et incidunt sequi et eius fugiat est temporibus similique rem illum natus sit unde eveniet.",
+            text3: "Non quos omnis ut autem labore nam vero consequatur est porro similique ad adipisci quisquam!"
+        },
+        {
+            id: 1,
+            image: RicardoImg,
+            author: "Ricardo Siqueira",
+            occupation: "Dev Back-end",
+            text1: "Lorem ipsum ",
+            text2: "dolor sit amet. Ex laboriosam dolorem non tempore earum et voluptatem suscipit ut cupiditate nisi est odit voluptates. Nam magni amet ut ipsam molestiae aut facilis minus et quia reiciendis sed excepturi rerum ex consequatur minima! Ex rerum sunt et incidunt officia et veritatis deserunt. Sit soluta laboriosam et incidunt sequi et eius fugiat est temporibus similique rem illum natus sit unde eveniet.",
+            text3: "Non quos omnis ut autem labore nam vero consequatur est porro similique ad adipisci quisquam!"
+        },
+    ]
     return(
         <main className={styles.main}>
 
             <Banner/>
 
             <div className={styles.comments}>
-                <CommentBox image={thaisImg} name="Thaís Gomes" occupation="Designer" time={1} text1="Lorem ipsum "
-                text2="dolor sit amet. Ex laboriosam dolorem non tempore earum et voluptatem suscipit ut cupiditate nisi est odit voluptates. Nam magni amet ut ipsam molestiae aut facilis minus et quia reiciendis sed excepturi rerum ex consequatur minima! Ex rerum sunt et incidunt officia et veritatis deserunt. Sit soluta laboriosam et incidunt sequi et eius fugiat est temporibus similique rem illum natus sit unde eveniet." 
-                text3="Non quos omnis ut autem labore nam vero consequatur est porro similique ad adipisci quisquam!"/>
-
-                <CommentBox image={RicardoImg} name="Ricardo Siqueira" occupation="Dev Back-end" time={1} text1="Lorem ipsum "
-                text2="dolor sit amet. Ex laboriosam dolorem non tempore earum et voluptatem suscipit ut cupiditate nisi est odit voluptates. Nam magni amet ut ipsam molestiae aut facilis minus et quia reiciendis sed excepturi rerum ex consequatur minima! Ex rerum sunt et incidunt officia et veritatis deserunt. Sit soluta laboriosam et incidunt sequi et eius fugiat est temporibus similique rem illum natus sit unde eveniet." 
-                text3="Non quos omnis ut autem labore nam vero consequatur est porro similique ad adipisci quisquam!"/>
+                {posts.map(post =>
+                    <CommentBox
+                    key={post.id}
+                    image={post.image}
+                    name={post.author}
+                    occupation={post.occupation}
+                    time={1}
+                    text1={post.text1}
+                    text2={post.text2}
+                    text3={post.text3}
+                    />
+                )}
             </div>
+            
         </main>
     )
 }
