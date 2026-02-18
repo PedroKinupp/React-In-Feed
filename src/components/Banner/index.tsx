eaa463c290e3e19afaa5e8e130ba2971f3a1a65a
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-02-18 18:14:35 -0300

    refactor: reajuste no html do componente banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,7 +2,7 @@ import styles from './styles.module.css'
 
 export default function Banner(){
     return (
-        <div className={styles.container}>
+        <div className={styles.banner}>
             <figure>
                 
             </figure>
