cb24b29aadc1835e92e4bc405aa43fc56bbb0787
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-02-18 19:50:58 -0300

    feat: ajuste no coment box para receber um array de feddbacks daquela postagem

--- src/components/CommentBox/index.tsx
+++ src/components/CommentBox/index.tsx
@@ -1,6 +1,14 @@
 import styles from './styles.module.css'
 import CommentButton from '../CommentButton/index.tsx'
-interface CommentBoxProps{
+import Feedbacks from '../Feedbacks/index.tsx';
+
+type feedback = {
+    author: string
+    time: number
+    content: string
+    likes: number
+}
+interface ICommentBoxProps{
     image: string;
     name: string;
     occupation: string;
@@ -8,9 +16,10 @@ interface CommentBoxProps{
     text1: string;
     text2: string;
     text3: string;
+    feedbacks: feedback[]
 }
 
-export default function CommentBox(props: CommentBoxProps){
+export default function CommentBox(props: ICommentBoxProps){
     return(
         <div className={styles.container}>
             <div>
@@ -35,6 +44,14 @@ export default function CommentBox(props: CommentBoxProps){
                 <textarea className={styles.textarea}></textarea>
                 <CommentButton/>
             </form>
+            {props.feedbacks.map((feedback) =>
+                <Feedbacks 
+                    author={feedback.author} 
+                    content={feedback.content}
+                    likes={feedback.likes}
+                    time={feedback.time}
+                />
+            )}
         </div>
     )
 }
