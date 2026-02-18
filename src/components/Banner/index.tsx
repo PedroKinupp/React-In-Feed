59aeec03b5c6a43692115625782765b25e267d83
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-02-17 23:09:22 -0300

    feat: add componente de banner pessoal

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.css'
+
+export default function Banner(){
+    return (
+        <div className={styles.container}>
+            <figure>
+                
+            </figure>
+            <div>
+                <h1>Pedro Kinupp</h1>
+                <h2>Dev Front-end</h2>
+            </div>
+        </div>
+    )
+}
