256ecc6619660b92ab38421663400b509b7cd47c
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-02-19 20:15:10 -0300

    refactor: reformulação das props dos feedbacks

--- src/components/Feedbacks/index.tsx
+++ src/components/Feedbacks/index.tsx
@@ -2,27 +2,28 @@ import styles from './styles.module.css'
 import LikeButton from '../Like'
 
 interface Ifeedbacks{
+    key: number
     image: string
     author: string
     time: number
     content: string
     likes: number
 }
-export default function Feedbacks({image, author, time, content, likes} : Ifeedbacks){
+export default function Feedbacks(props : Ifeedbacks){
     return(
         <div className={styles.feedbacks}>
             <figure>
-                <img src={image}/>
+                <img src={props.image}/>
             </figure>
             <div className={styles.container}>
                 <div className={styles.content}>
                     <div>
-                        <h2>{author}</h2>
-                        <h3>Cerca de {time}h</h3>
+                        <h2>{props.author}</h2>
+                        <h3>Cerca de {props.time}h</h3>
                     </div>
-                    <p>{content}</p>
+                    <p>{props.content}</p>
                 </div>
-                <LikeButton likes={likes}/>
+                <LikeButton likes={props.likes}/>
             </div>
         </div>
     )
