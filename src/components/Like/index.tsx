0d5c4bd319d0b45738109caf61a973788fab876d
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-02-19 21:06:28 -0300

    style: like fica azul quando o usuário ja deu like

--- src/components/Like/index.tsx
+++ src/components/Like/index.tsx
@@ -1,5 +1,6 @@
 import styles from './styles.module.css'
 import like from "../../assets/like.png"
+import notlike from '../../assets/notlike.png'
 import { useRef, useState } from 'react'
 
 type propstype = {
@@ -8,25 +9,33 @@ type propstype = {
 
 export default function LikeButton(props : propstype){
     const [LikeNum, setLikeNum] = useState(props.likes)
+    const [icon, setIcon] = useState(notlike)
+    const [textColor, setTextcolor] = useState("#DDDDDD")
 
     const Liked = useRef(false)
 
     function handleLike(){
         if(!Liked.current){
             setLikeNum(LikeNum + 1)
+            setIcon(like)
+            setTextcolor("#007DFA")
             Liked.current = true
         } else{
             setLikeNum(LikeNum - 1)
+            setIcon(notlike)
+            setTextcolor("#DDDDDD")
             Liked.current = false
         }
     }
 
     return(
-        <div className={styles.like}>
+        <div 
+            className={styles.like}>
             <button
                 onClick={handleLike}
+                style={{ color: textColor }}
             >
-                <img src={like}/>
+                <img src={icon}/>
                 Like • {LikeNum}
             </button>
         </div>
